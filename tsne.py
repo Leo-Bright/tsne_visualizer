b77ff45ffb319fd2ba147f31ae4074c8fbe6d127
Leo Bright <buxingzhexiong@sina.com>
2020-05-22 22:56:50 +0800

    bug fix

--- tsne.py
+++ tsne.py
@@ -6,11 +6,13 @@ from sklearn.manifold import TSNE
 def save_embeddings(embeddings, label, output_file_path):
 
     with open(output_file_path, 'w+') as f:
-        for idx, id_emb in range(len(embeddings)):
+        idx = 0
+        for id_emb in embeddings:
             f.write(id_emb[0])
             f.write(' '.join(map(str, id_emb[1])))
             f.write(' ' + label[idx])
             f.write('\n')
+            idx += 1
 
 
 if __name__ == '__main__':
