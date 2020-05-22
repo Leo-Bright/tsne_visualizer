790ce788addb5b903aeceaea2992eb46e56d36a6
Leo Bright <buxingzhexiong@sina.com>
2020-05-22 22:52:57 +0800

    增加保存embedding代码

--- tsne.py
+++ tsne.py
@@ -3,23 +3,28 @@ import pandas as pd
 from sklearn.manifold import TSNE
 
 
-def save_embeddings(embeddings, output_file_path):
+def save_embeddings(embeddings, label, output_file_path):
+
     with open(output_file_path, 'w+') as f:
-        for id_emb in embeddings:
+        for idx, id_emb in range(embeddings):
             f.write(id_emb[0])
             f.write(' '.join(map(str, id_emb[1])))
+            f.write(' ' + label[idx])
             f.write('\n')
 
 
 if __name__ == '__main__':
 
-    emb_file_path = 'data/sanfrancisco_raw_feature_crossing.embeddings'
+    emb_file_path = 'data/sanfrancisco_raw_feature_crossing_labeled.embeddings'
     tsne_emb_path = 'data/sanfrancisco_raw_feature_crossing_tsne.embeddings'
     # df = pd.read_csv(emb_file_path)
     df = pd.read_csv(emb_file_path, header=None, sep=' ', index_col=0)
     X = df
 
+    rows, cols = df.shape
+    label = df[cols]
+    del df[cols]
+
     X_embedded = TSNE(n_components=2).fit_transform(X)
-    print(X_embedded.shape)
-    print(X_embedded)
-    save_embeddings(X_embedded, tsne_emb_path)
+
+    save_embeddings(X_embedded, label, tsne_emb_path)
