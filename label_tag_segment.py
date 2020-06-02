b8b93ce55e482c659ab8f6143ac7541e16348d22
Leo Bright <564312023@qq.com>
2020-06-02 14:54:06 +0800

    增加segment可视化代码

--- label_tag_segment.py
+++ label_tag_segment.py
@@ -1,9 +1,11 @@
 import json
 
 
-def label_embeddings(embeddings, output, seg2tag, label='Ave'):
+def label_embeddings(embeddings, output, seg2tag, labels):
 
     seg2emb = {}
+    tag_conut = {}
+
     normal_count = 0
     unnormal_count = 0
 
@@ -19,15 +21,19 @@ def label_embeddings(embeddings, output, seg2tag, label='Ave'):
         for key in seg2emb:
             emb = seg2emb[key]
 
-            if key in seg2tag and seg2tag[key] == label:
+            if key in seg2tag and seg2tag[key] in labels:
                 tag = seg2tag[key]
-                f.write(key + ' %s' % ' '.join(emb) + ' normal\n')
+                f.write(key + ' %s' % ' '.join(emb) + ' ' + tag + '\n')
+                if tag not in tag_conut:
+                    tag_conut[tag] = 0
+                tag_conut[tag] += 1
                 normal_count += 1
-            elif unnormal_count < normal_count + 1:
+            elif unnormal_count < (normal_count + 1)/(len(labels)-1):
                 f.write(key + ' %s' % ' '.join(emb) + ' unnormal\n')
                 unnormal_count += 1
 
-    print('normal count:', normal_count)
+    for key in tag_conut:
+        print(key + " count: ", str(tag_conut[key]))
     print('unnormal count:', unnormal_count)
 
 
@@ -49,9 +55,10 @@ def gen_seg_tag_dict(tag_file_path, label_set):
 
 if __name__ == '__main__':
 
-    seg_tag_dict = gen_seg_tag_dict('data/segment/sf_segments_tiger_nametype.json', ('Ave',))
-
+    labels = ('Ave', 'unnormal')
     src_emb_file_path = 'data/segment/sanfrancisco_raw_feature_segment.embeddings'
     labeled_emb_path = 'data/segment/sanfrancisco_raw_feature_segment_labeled.embeddings'
 
-    label_embeddings(src_emb_file_path, labeled_emb_path, seg_tag_dict, label='Ave')
+    seg_tag_dict = gen_seg_tag_dict('data/segment/sf_segments_tiger_nametype.json', labels)
+
+    label_embeddings(src_emb_file_path, labeled_emb_path, seg_tag_dict, labels)
---
+++ my_visualizer_segment2.py
@@ -0,0 +1,55 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+labels = ('Ave', 'unnormal')
+colors = ('darkviolet', 'brown')
+
+emb_path = 'data/segment/sanfrancisco_raw_feature_segment_labeled_tsne.embeddings'
+x_location = (-30, 15)
+y_location = (-30, 10)
+
+sf_X = {}
+sf_Y = {}
+sf_T = {}
+
+for i in range(len(labels)):
+    sf_T[labels[i]] = colors[i]
+
+with open(emb_path) as f:
+
+    for line in f:
+        x_y_label = line.strip().split(' ')
+        x = float(x_y_label[0])
+        y = float(x_y_label[1])
+        label = x_y_label[-1]
+
+        if label not in sf_X:
+            sf_X[label] = []
+        sf_X[label].append(x)
+
+        if label not in sf_Y:
+            sf_Y[label] = []
+        sf_Y[label].append(y)
+
+for label in sf_X:
+    sf_X[label] = np.array(sf_X[label])
+    sf_Y[label] = np.array(sf_Y[label])
+
+# 散点图
+scatters = []
+for label in labels:
+    _scatter = plt.scatter(sf_X[label], sf_Y[label], s=5, c=sf_T[label], alpha=.5)
+    scatters.append(_scatter)
+
+plt.legend(handles=scatters,
+           labels=labels,
+           loc='upper right')
+
+plt.xlim(x_location[0], x_location[1])
+plt.xticks(())  # ignore xticks
+plt.ylim(y_location[0], y_location[1])
+plt.yticks(())  # ignore yticks
+
+plt.show()
+
